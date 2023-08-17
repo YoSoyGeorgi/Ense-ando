094d96924d4a61500ba0513048b837145fae2dbb
Jorge Luis Olvera <holatosoygeorgi@outlook.com>
2023-08-17 15:47:44 -0600

    Primera version

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="styles/style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+    <header class="header">
+
+        <a href="#" class="logo">
+            <img src="img/LOGO Enseñando.png" alt="Logo" style="width: 100px;">
+        </a>
+        <input type="checkbox" id="check">
+
+        <label for="check" class="icons">
+            <i class='bx bx-menu' id="menu-icon"></i>
+            <i class='bx bx-x' id="close-icon"></i>
+        </label>
+
+        <ul class="navbar">
+            <li><a href="#">Inicio</a></li>
+            <li>
+                <a href="#">Capacitaciones</a>
+                <ul class="dropdown">
+                    <li><a href="#">Educativa</a></li>
+                    <li><a href="#">Competencias</a></li>
+                    <li><a href="#">Empresarial</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Contactanos</a></li>
+            <li><a href="#">Iniciar sesión</a></li>
+        </ul>
+
+
+    </header>
+
+</body>
+
+</html>
---
+++ scripts/script.js
@@ -0,0 +1,4 @@
+// Dentro de tu archivo script.js
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.navbar').classList.toggle('menu-active');
+  });
